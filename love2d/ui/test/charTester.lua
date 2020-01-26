4e020a1524b4dcfa1d88e042d920e084a41fec6a
Paul E. West <paulofthewest@gmail.com>
2020-01-26 18:39:50 -0500

    Added a debug run facility
    + Passing "-d <testname>" to UI will run a specific test.
    + This should allow us to test individual pieces of the UI.

---
+++ love2d/ui/test/charTester.lua
@@ -0,0 +1,10 @@
+local charTester = {}
+-- This loads the C library and binds is to a 'ds' object
+local ds = require 'libds'
+dsl = {}
+
+function charTester.run()
+    print ("END CHARACTER TEST")
+end
+
+return charTester
