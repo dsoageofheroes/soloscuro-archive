a5ff6f9d966017adacddb823be98aa0399eaf1d6
RandomContributions <79341663+RandomContributions@users.noreply.github.com>
2021-02-21 22:16:23 -0700

    Updated paths for dofile()

--- lua/main.lua
+++ lua/main.lua
@@ -2,12 +2,12 @@ function init()
     ds.load_charsave(0, 15) -- load character from charsave.gff
     ds.load_charsave(1, 16) -- load character from charsave.gff
 
-    ds.set_ignore_repeat(true) -- sat to false if you want repeats
+    ds.set_ignore_repeat(true) -- set to false if you want repeats
     ds.load_region(42) -- right now only 42 works (there is some hard-coding.)
 
     return true -- returning true, means that lua has setup the game, don't do the default
                 -- returning false, would allow the engine to proceed with normal startup
 end
 
-dofile("player_input.lua")
-dofile("settings.lua")
+dofile("./lua/player_input.lua")
+dofile("./lua/settings.lua")
