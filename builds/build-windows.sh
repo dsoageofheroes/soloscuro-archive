7482e9f0f968379a1ad888878ab9557c31d65ffa
www-data <www-data@dso.netgear.com>
2021-02-22 01:58:51 +0000

    build fix

--- builds/build-windows.sh
+++ builds/build-windows.sh
@@ -9,9 +9,9 @@ make clean
 CC=x86_64-w64-mingw32-gcc make -f makefile.win mdark
 cd builds
 rm -rf staging
-mkdir staging
+mkdir -p staging/lua
 cp ../mdark.exe staging/
-cp ../*.lua staging/
+cp ../lua/*.lua staging/lua/
 cp ../*dll staging/
 locate SDL2.dll | xargs -I {} cp {} staging/
 locate lua53.dll | xargs -I {} cp {} staging/
