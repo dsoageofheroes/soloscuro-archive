ba0fc9d5cc17366d578ad0c7f56d241c831ad1a7
RandomContributions <79341663+RandomContributions@users.noreply.github.com>
2021-02-21 19:26:39 -0700

    Lua changes
    Moved settings (except for the key repeat thing, which we might want to remove from Lua) to settings.lua.
    
    player_input.lua now calls the is_[modifier]_down() function just once at the beginning of keydown/keyup. Changed function variables to local scope.

---
+++ lua/settings.lua
@@ -0,0 +1,6 @@
+-- Movement Options
+ds.set_player_frame_delay(7) -- set how many delay ticks between frame changes of a player sprite. (default: 7)
+ds.set_player_move(10) -- set how many ticks to move one slot. (default: 10)
+
+-- Debug/Developer Options
+--ds.set_quiet(1) -- uncomment to place into quiet mode
