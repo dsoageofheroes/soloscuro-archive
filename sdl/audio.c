a0e127a0569496fa3d2e7acbf317a61b1d961c86
Paul E. West <paulofthewest@gmail.com>
2021-03-22 10:02:43 -0400

    Added looping to midi playback.

--- sdl/audio.c
+++ sdl/audio.c
@@ -10,9 +10,10 @@ static uint8_t xmi_buf[1<<13];
 static SDL_AudioSpec            spec, obtained;
 static struct ADL_MIDIPlayer    *midi_player = NULL;
 static struct ADLMIDI_AudioFormat audio_format;
+static int midi_gff = -1, midi_type, midi_res_id;
 
 // The MIDI and VOC subsystems generate sounds at different volumes.
-// Namely voc is super loud and midi is quiet. These factor are applied to the volumes.
+// Namely voc is super loud and midi is quiet. These factor are applied to equalize the volumes.
 #define MIDI_FACTOR (6.0)
 #define VOC_FACTOR (0.2)
 
@@ -108,22 +109,25 @@ extern void audio_init() {
 
     adl_setVolumeRangeModel(midi_player, ADLMIDI_VolumeModel_AIL);
     adl_setScaleModulators(midi_player, 1);
+    //adl_setLoopEnabled(midi_player, 1);
 }
 
-extern void audio_play_xmi(const int gff_idx, uint32_t type, uint32_t res_id) {
+static void load_midi() {
     char *buf = NULL;
+
+    if (midi_gff < 0) { return; }
 
     SDL_PauseAudio(1);
     if (xmi_playing) {
         adl_reset(midi_player);
     }
 
-    gff_chunk_header_t chunk = gff_find_chunk_header(gff_idx, type, res_id);
+    gff_chunk_header_t chunk = gff_find_chunk_header(midi_gff, midi_type, midi_res_id);
     buf = malloc(chunk.length);
-    gff_read_chunk(gff_idx, &chunk, buf, chunk.length);
+    gff_read_chunk(midi_gff, &chunk, buf, chunk.length);
 
     if (adl_openData(midi_player, buf, chunk.length)) {
-        error("Couldn't open music entry (%d, %d, %d): %s\n", gff_idx, type, res_id, adl_errorInfo(midi_player));
+        error("Couldn't open music entry (%d, %d, %d): %s\n", midi_gff, midi_type, midi_res_id, adl_errorInfo(midi_player));
         return;
     }
 
@@ -132,6 +136,13 @@ extern void audio_play_xmi(const int gff_idx, uint32_t type, uint32_t res_id) {
     SDL_PauseAudio(0);
 
     free(buf);
+}
+
+extern void audio_play_xmi(const int gff_idx, uint32_t type, uint32_t res_id) {
+    midi_gff = gff_idx;
+    midi_type = type;
+    midi_res_id = res_id;
+    load_midi();
 }
 
 static sf_count_t vfget_filelen (void *user_data) {
@@ -317,6 +328,11 @@ static void soloscuro_audio_callback(void *midi_player, uint8_t *stream, int len
 
     /* Convert bytes length into total count of samples in all channels */
     int samples_count = len / audio_format.containerSize;
+
+    //printf("midi_position = %f / %f\n", adl_positionTell(midi_player), adl_totalTimeLength(midi_player));
+    if (adl_atEnd(midi_player)) {
+        adl_positionRewind(midi_player);
+    }
 
     /* Take some samples from the ADLMIDI */
     samples_count = adl_playFormat(p, samples_count,
---
+++ src/powers.h
@@ -0,0 +1,56 @@
+#ifndef POWERS_H
+#define POWERS_H
+
+#include "entity.h"
+
+enum power_shape {
+    POWER_SINGLE, // a point, aoe is ignore
+    POWER_CIRCLE, // EX: Fireball
+    POWER_CONE,   // EX: Cone of Cold
+    POWER_RECTANGLE, // aoe is two 8-bit ints, width times height
+    POWER_MULTI,  // aoe is number of charges.
+};
+
+struct power_instance_s;
+// A power is a spell, psionic, innate special ability, or charge on an item.
+// This should be stored in a general list/array/hashtable for access at any time.
+typedef struct power_s {
+    char          *name;
+    char          *description;
+    uint16_t      range;
+    uint16_t      aoe;
+    enum          power_shape shape;
+    sprite_info_t icon;
+    int           (*can_activate) (struct power_instance_s *source);
+    int           (*pay)          (struct power_instance_s *source);
+    int           (*apply)        (struct power_instance_s *source, entity_t *target);
+    int           (*still_active) (struct power_instance_s *source, const int rounds_past);
+    int           (*affect_power) (struct power_instance_s *target);
+    // Add animation sequence info here.
+} power_t;
+
+// This (power/spell entity) is an instance of the spell on a tile in the region. EX: Grease
+// During creation a COPY of the entity or item is made, but only one per group.
+typedef struct power_instance_s {
+    entity_t *entity;
+    item_t   *item;
+    power_t  *stats;
+    struct power_instance_s *next, *prev;
+} power_instance_t;
+
+typedef struct power_instance_list_s {
+    power_instance_t *head;
+} power_instance_list_t;
+
+// List of all the spell nodes on a region.
+typedef struct power_overlay_s {
+    power_instance_list_t *power_list[MAP_ROWS][MAP_COLUMNS];
+} power_overlay_t;
+
+// Okay before I forget.
+// Spells create a spell entity on point of impact.
+// If AOE, then many children entities are created that point to the parent/master spell entity.
+// This allows each tile/square its own trigger and the parent/master can clean up the children when the duration is over.
+
+
+#endif
